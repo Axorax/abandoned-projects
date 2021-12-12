294aa207ae1893062b0dbb5d49116985b2c7a895
Axorax <78349410+Axorax@users.noreply.github.com>
2021-12-12 17:30:13 +0600

    Add files via upload

---
+++ SODL Website l HTML,CSS,JS/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+const hamburgerImg = document.querySelector('.hamburger-icon-closed');
+const hamburgerBtn = document.querySelector('.hamburger-menu');
